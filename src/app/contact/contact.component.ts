2e0a8a4f81cd3f315c4dfb4ef69136b96af4e7e5
nikfrankson <nikfrankson@mail.fresnostate.edu>
2019-12-05 15:14:49 -0800

    functional code

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -6,6 +6,14 @@ import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
 import { ToastService } from '../toast/toast.service';
 
+export interface IContact {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  owed: number;
+}
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -13,17 +21,66 @@ import { ToastService } from '../toast/toast.service';
 })
 export class ContactComponent implements OnInit {
 
-  contacts: Array<any> = [];
+  contacts: Array<IContact> = [];
   constructor(
     private http: Http,
     private activatedRoute: ActivatedRoute,
     private router: Router,
-    private toastService: ToastService
+    private toastService: ToastService,
   ) {
   }
 
   async ngOnInit() {
+  }
+
+
+  async loadContacts() {
+    const contacts = await this.http.get('assets/contacts.json').toPromise();
+    this.contacts = contacts.json();
+    return contacts.json();
+  }
+
+  addContacts() {
+    const contacts: IContact = {
+      id: null,
+      firstName: null,
+      lastName: null,
+      email: null,
+      phone: null,
+      owed: null
+    };
+    this.contacts.push(contacts);
+    this.saveToLocalStorage();
+  }
+  delete(index: number) {
+    this.contacts.splice(index, 1);
+    console.log('index ', index);
+  }
+
+finalize() {
+  const data = this.calculate();
+  this.router.navigate(['home', data]);
+}
+
+  calculate() {
+    let owed = 0;
+    for (let i = 0; i < this.contacts.length; i++) {
+      owed += this.contacts[i].owed;
+    } return {
+      numberOfContacts: this.contacts.length,
+      subTotal: owed,
+      taxAmount: owed * .12,
+      total: owed + (owed * .12)
+    };
+
 
   }
+
+
+  saveToLocalStorage() {
+    localStorage.setItem('contacts', JSON.stringify(this.contacts));
+  }
+
+
 
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,17 +3,40 @@ import { LocalStorageService } from '../localStorageService';
 import { Router } from '@angular/router';
 import { ToastService } from '../toast/toast.service';
 
+export interface IUser {
+  id?: number;
+  username: string;
+  password: string;
+}
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  user: IUser = {
+    username: null,
+    password: null,
+  };
   constructor(private router: Router, private toastService: ToastService) {
   }
 
   ngOnInit() {
 
+  }
+  login(user: IUser) {
+    const presetUser = {username: 'admin', password: 'password'};
+    if (this.user.username != null && this.user.password != null &&
+      this.user.username !== '' && this.user.password !== '') {
+        if (this.user.username === presetUser.username && this.user.password === presetUser.password) {
+          localStorage.getItem(JSON.stringify(this.user));
+          this.router.navigate(['contacts', this.user]);
+        } else {
+          this.toastService.showToast('warning', 2000, 'Username or Password is incorrect!');
+        }
+      } else {
+        this.toastService.showToast('danger', 2000, 'Must type in credentials!');
+      }
   }
 
 }
